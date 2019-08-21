88d069ba9bce61d16c7ad0c247d7aa792eb43a20
RunningSnail <xp406181@163.com>
2019-08-21 13:57:57 +0800

    逃生可以结束，加入石像鬼

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
+
+    bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
+    float m_Timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_IsPlayerAtExit)
+        {
+            EndLevel (exitBackgroundImageCanvasGroup);
+        }
+        else if(m_IsPlayerCaught)
+        {
+            EndLevel (caughtBackgroundImageCanvasGroup);
+        }
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    void EndLevel (CanvasGroup imageCanvasGroup)
+    {
+        m_Timer += Time.deltaTime;
+
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if(m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit ();
+        }
+    }
+}
---
+++ Assets/Scripts/Observer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    public Transform player;
+    public GameEnding gameEnding;
+    bool m_IsPlayerInRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_IsPlayerInRange)
+        {
+            Vector3 direction = player.position - transform.position + Vector3.up;
+            Ray ray = new Ray (transform.position, direction);
+            RaycastHit raycastHit;
+            if(Physics.Raycast(ray, out raycastHit))
+            {
+                if(raycastHit.collider.transform == player)
+                {
+
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if(other.transform == player)
+        {
+            m_IsPlayerInRange = true;
+        }
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        if(other.transform == player)
+        {
+            m_IsPlayerInRange = false;
+        }
+    }
+}
