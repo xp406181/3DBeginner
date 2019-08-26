57df59c81280d0ebdbd8bfa617b34fd885b0b4c6
xp406181 <584018312@qq.com>
2019-08-26 13:40:18 +0800

    增加巡逻怪物

--- Assets/Scripts/Observer.cs
+++ Assets/Scripts/Observer.cs
@@ -25,7 +25,7 @@ public class Observer : MonoBehaviour
             {
                 if (raycastHit.collider.transform == player)
                 {
-
+                    gameEnding.CaughtPlayer();
                 }
             }
         }
